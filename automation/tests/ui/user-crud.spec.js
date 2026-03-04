8c7608754493ed1b16a14eee4db19c9f607e71c2
Borirat <Boriratkk@gmail.com>
2026-03-05 00:09:31 +0700

    update Jenkind 3

--- automation/tests/ui/user-crud.spec.js
+++ automation/tests/ui/user-crud.spec.js
@@ -1,14 +1,16 @@
-const { test, expect } = require('@playwright/test')
+import { test, expect } from '@playwright/test'
 
 test('create user', async ({ page }) => {
 
- await page.goto('/')
+  const name = 'tester' + Date.now()
 
- await page.fill('input[placeholder="name"]','tester' + Date.now())
- await page.fill('input[placeholder="email"]','tester@test.com')
+  await page.goto('http://localhost:3000')
 
- await page.click('text=create')
+  await page.fill('#name', name)
+  await page.fill('#email', `${name}@test.com`)
 
- await expect(page.locator('text=tester')).toBeVisible()
+  await page.click('text=create')
+
+  await expect(page.getByText(name)).toBeVisible()
 
 })
